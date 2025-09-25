836509393bd42955c061d1c3e926b48d03b940d1
Lucas <lucashammerstad@gmail.com>
2025-09-25 09:53:02 +0200

    Made a simple UI as a start for the development. Have begun setting up the camera functionality

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter , RouterProvider } from "react-router-dom";
 import App from './App.tsx'
+import BarcodeButton from './components/BarcodeButton';
+import "./style.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+    { path: "/", element: <App /> },
+    { path: "/scan", element: <BarcodeButton /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router = {router} />
+    </React.StrictMode>
 )
